91b94206936147300a5ae269d0a3b76007a55ac9
Toby Tripp <toby.tripp+github@gmail.com>
2018-08-09 11:37:40 -0500

    Change cider to last _stable_ version

--- boot/profile.boot
+++ boot/profile.boot
@@ -1,6 +1,6 @@
 (require 'boot.repl)
 (swap! boot.repl/*default-dependencies*
-       concat '[[cider/cider-nrepl "0.18.0-SNAPSHOT"]])
+       concat '[[cider/cider-nrepl "0.17.0"]])
 
 (swap! boot.repl/*default-middleware*
        concat '[cider.nrepl/cider-middleware])
--- lein/project.clj
+++ lein/project.clj
@@ -1,11 +1,19 @@
 (defproject clj-builder "1.0.0"
   :description "Generate Docker containers for building clojure projects"
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
+  :license {:name         "Eclipse Public License - v 1.0"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as Clojure"}
+            :comments     "same as Clojure"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure         "1.9.0"]
+                 [environ                     "1.1.0"]
+                 [com.stuartsierra/component  "0.3.2"]
+
+                 [com.taoensso/timbre         "4.10.0"]
+                 [metosin/compojure-api       "2.0.0-alpha21"]
+                 [http-kit                    "2.2.0"]
+                 [buddy/buddy-auth            "2.1.0"]
+                 [hiccup                      "1.0.5"]]
 
   :main ^:skip-aot builder.core
 
@@ -16,9 +24,10 @@
 
   :plugins        [[lein-pprint "1.1.1"]]
 
-  :profiles {:repl {:dependencies [[cider/cider-nrepl "0.18.0-SNAPSHOT"]]}
+  :profiles {:dev     {:plugins [[lein-ring "0.10.0"]]}
+             :repl    {:dependencies [[cider/cider-nrepl "0.17.0"]]}
              :uberjar {:aot :all}}
   :global-vars {*warn-on-reflection* true
-                *assert* true}
+                *assert*             true}
   :jvm-opts ["-Xmx1g"]
   :bootclasspath true)
